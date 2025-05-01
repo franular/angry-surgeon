baf94894d957ff378d6cf275ab2c6868b823f414
vinsynth <1.5vhunt@gmail.com>
2025-05-01 18:19:40 -0400

    daisy init

---
+++ angry-surgeon-stm32h7/flash.sh
@@ -0,0 +1,7 @@
+USB_PID=df11
+FLASH_ADDRESS=0x08000000
+# SRAM_ADDRESS=0x24000000
+# QSPI_ADDRESS=0x90040000
+APP_BIN=target/ansuz.bin
+cargo objcopy --release -- -O binary $APP_BIN
+dfu-util -a 0 -s $FLASH_ADDRESS:leave -D $APP_BIN -d ,0483:$USB_PID
